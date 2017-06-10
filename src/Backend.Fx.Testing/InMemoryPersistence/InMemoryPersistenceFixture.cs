e547dfe774c219b3986d3ab339d77d342dac0fce
marcwittke <marc@wittke-web.de>
2017-06-10 13:49:29 -0300

    InMemory Persistence and Id Generation

--- src/Backend.Fx.Testing/InMemoryPersistence/InMemoryPersistenceFixture.cs
+++ src/Backend.Fx.Testing/InMemoryPersistence/InMemoryPersistenceFixture.cs
@@ -7,18 +7,20 @@
     using Environment.DateAndTime;
     using Environment.MultiTenancy;
     using Patterns.Authorization;
+    using Patterns.IdGeneration;
     using Patterns.UnitOfWork;
     using RandomData;
 
-    public abstract class InMemoryPersistenceFixture
+    public abstract class InMemoryPersistenceFixture : IIdGenerator
     {
         private readonly Dictionary<Type, object> stores;
+        private int nextId;
 
         protected InMemoryPersistenceFixture(bool withDemoData, Assembly domainAssembly)
         {
             using (var dataGenerationRuntime = new DataGenerationRuntime(domainAssembly))
             {
-                dataGenerationRuntime.Boot();
+                dataGenerationRuntime.Boot(container => container.RegisterSingleton<IIdGenerator>(this));
                 TenantId = withDemoData
                                ? dataGenerationRuntime.TenantManager.CreateDemonstrationTenant("test", "", false)
                                : dataGenerationRuntime.TenantManager.CreateProductionTenant("test", "", false);
@@ -52,6 +54,11 @@
         {
             public void Flush()
             { }
+        }
+
+        public int NextId()
+        {
+            return nextId++;
         }
     }
 }
