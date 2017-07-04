e488758b13723b11f3c3b5754cf5238d7f8b4948
marcwittke <marc@wittke-web.de>
2017-07-04 12:46:58 -0300

    New ConflictedException --> for HTTP 409

---
+++ src/Backend.Fx/Exceptions/ConflictedException.cs
@@ -0,0 +1,16 @@
+﻿namespace Backend.Fx.Exceptions
+{
+    using System;
+
+    public class ConflictedException : ClientException
+    {
+        public ConflictedException()
+        { }
+
+        public ConflictedException(string message) : base(message)
+        { }
+
+        public ConflictedException(string message, Exception innerException) : base(message, innerException)
+        { }
+    }
+}
