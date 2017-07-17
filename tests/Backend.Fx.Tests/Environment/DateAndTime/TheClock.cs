2c7506e9639be2a3c6e35777079632d5dc891a8e
marcwittke <marc@wittke-web.de>
2017-07-16 22:13:26 -0300

    test fix

--- tests/Backend.Fx.Tests/Environment/DateAndTime/TheClock.cs
+++ tests/Backend.Fx.Tests/Environment/DateAndTime/TheClock.cs
@@ -7,7 +7,6 @@ namespace Backend.Fx.Tests.Environment.DateAndTime
 
     public class TheClock
     {
-        private const int BuenosAiresUtcOffset = -180;
         [Fact]
         public void AllowsOverridingOfUtcNow()
         {
@@ -18,34 +17,6 @@ namespace Backend.Fx.Tests.Environment.DateAndTime
             Assert.Equal(overriddenUtcNow, sut.UtcNow);
             Thread.Sleep(100);
             Assert.Equal(overriddenUtcNow, sut.UtcNow);
-        }
-
-        [Fact]
-        public void CanConvertToLocalTime()
-        {
-            var overriddenUtcNow = new DateTime(2000, 1, 1, 12, 0, 0);
-            Clock sut = new WallClock();
-            sut.OverrideUtcNow(overriddenUtcNow);
-            var localNow = sut.GetLocalNow(BuenosAiresUtcOffset);
-            Assert.Equal(new DateTime(2000, 1, 1, 9, 0, 0), localNow);
-        }
-
-        [Fact]
-        public void CanLocalizeUtcMoment()
-        {
-            var moment = new DateTime(2000, 1, 1, 12, 0, 0);
-            Clock sut = new WallClock();
-            var localNow = sut.LocalizeUtcDateTime(moment, BuenosAiresUtcOffset);
-            Assert.Equal(new DateTime(2000, 1, 1, 9, 0, 0), localNow);
-        }
-
-        [Fact]
-        public void CanConvertLocalMomentToUtc()
-        {
-            var moment = new DateTime(2000, 1, 1, 12, 0, 0);
-            Clock sut = new WallClock();
-            var utcNow = sut.ToUtcTime(moment, BuenosAiresUtcOffset);
-            Assert.Equal(new DateTime(2000, 1, 1, 15, 0, 0), utcNow);
         }
     }
 }
