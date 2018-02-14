cff346be8a4ce88e07155f47390767e4d3163278
Marc Wittke <marc@wittke-web.de>
2018-02-13 23:08:25 -0300

    version 2.0

--- demo/DemoBlog.Mvc/Infrastructure/Bootstrapping/BlogPersistenceModule.cs
+++ demo/DemoBlog.Mvc/Infrastructure/Bootstrapping/BlogPersistenceModule.cs
@@ -2,11 +2,19 @@
 {
     using Backend.Fx.Bootstrapping;
     using Backend.Fx.Bootstrapping.Modules;
+    using Backend.Fx.Patterns.IdGeneration;
     using Microsoft.EntityFrameworkCore;
     using Persistence;
+    using SimpleInjector;
 
     public class BlogPersistenceModule : EfCorePersistenceModule<BlogDbContext> {
         public BlogPersistenceModule(SimpleInjectorCompositionRoot compositionRoot, DbContextOptions<BlogDbContext> dbContextOptions) : base(compositionRoot, dbContextOptions)
         { }
+
+        protected override void Register(Container container, ScopedLifestyle lifestyle)
+        {
+            base.Register(container, lifestyle);
+            container.RegisterSingleton<IEntityIdGenerator, BlogEntityIdGenerator>();
+        }
     }
 }
