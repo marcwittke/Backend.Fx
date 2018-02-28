77e34f9c63db458f0a63067fc6075a98aef68849
Marc Wittke <marc@wittke-web.de>
2018-02-28 13:29:00 -0300

    AssemblyInfo cleanup

---
+++ src/Backend.Fx.Testing/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyVersion("0.0.0.0")]
+[assembly: AssemblyFileVersion("0.0.0.0")]
+[assembly: AssemblyInformationalVersion("0.0.0.0")]
