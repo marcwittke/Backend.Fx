87f3094b29ed94c00c6ef424643832e54ae35728
Marc Wittke <marc@wittke-web.de>
2018-05-16 11:50:08 -0300

    In Memory Event Bus More Logging

--- src/Backend.Fx/Patterns/EventAggregation/Integration/IEventBus.cs
+++ src/Backend.Fx/Patterns/EventAggregation/Integration/IEventBus.cs
@@ -7,6 +7,7 @@
     using DependencyInjection;
     using Environment.Authentication;
     using Environment.MultiTenancy;
+    using Logging;
     using UnitOfWork;
 
     public interface IEventBus
@@ -22,11 +23,13 @@
 
     public abstract class EventBus : IEventBus
     {
+        private static readonly ILogger Logger = LogManager.Create<EventBus>();
         private readonly IScopeManager scopeManager;
         private readonly ConcurrentDictionary<string, List<Type>> subscriptions = new ConcurrentDictionary<string, List<Type>>();
 
         protected EventBus(IScopeManager scopeManager)
         {
+            Logger.Debug($"Instantiating {GetType()}");
             this.scopeManager = scopeManager;
         }
 
@@ -34,10 +37,12 @@
 
         public void Subscribe<THandler>(string eventName) where THandler : IIntegrationEventHandler
         {
+            Logger.Info($"Subscribing to {eventName}");
             var type = typeof(THandler);
-            subscriptions.AddOrUpdate(eventName, 
-                                      s => new List<Type> { type }, 
-                                      (s, list) => { 
+            subscriptions.AddOrUpdate(eventName,
+                                      s => new List<Type> { type },
+                                      (s, list) =>
+                                      {
                                           list.Add(type);
                                           return list;
                                       });
@@ -46,6 +51,7 @@
 
         public void Unsubscribe<THandler>(string eventName) where THandler : IIntegrationEventHandler
         {
+            Logger.Info($"Unsubscribing from {eventName}");
             subscriptions.TryRemove(eventName, out var _);
             Unsubscribe(eventName);
         }
@@ -56,6 +62,7 @@
 
         protected virtual async Task Process(string eventName, object rawEventPayload)
         {
+            Logger.Info($"Processing a {eventName} event");
             if (subscriptions.TryGetValue(eventName, out List<Type> handlerTypes))
             {
                 IntegrationEventData integrationEvent = Deserialize(rawEventPayload);
@@ -65,18 +72,31 @@
                     {
                         using (var unitOfWork = scope.GetInstance<IUnitOfWork>())
                         {
-                            unitOfWork.Begin();
-
-                            var dynamicHandler = (IIntegrationEventHandler)scope.GetInstance(handlerType);
-                            await dynamicHandler.Handle(integrationEvent.Event);
-
-                            unitOfWork.Complete();
+                            try
+                            {
+                                unitOfWork.Begin();
+                                Logger.Info($"Getting subscribed handler instance of type {handlerType.Name}");
+                                var handler = (IIntegrationEventHandler)scope.GetInstance(handlerType);
+                                using (Logger.InfoDuration($"Invoking subscribed handler {handler.GetType().Name}"))
+                                {
+                                    await handler.Handle(integrationEvent.Event);
+                                }
+                                unitOfWork.Complete();
+                            }
+                            catch (Exception ex)
+                            {
+                                Logger.Error(ex, $"Handling of {eventName} by {handlerType} failed: {ex.Message}");
+                            }
                         }
                     }
                 }
             }
+            else
+            {
+                Logger.Info($"No handler registered. Ignoring {eventName} event");
+            }
         }
-        
+
         protected class IntegrationEventData
         {
             public IntegrationEventData(TenantId tenantId, dynamic @event)
