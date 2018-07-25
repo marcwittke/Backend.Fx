434032257ee8f9050f54db1a9ebb93e357d063ae
Marc Wittke <marc@wittke-web.de>
2018-07-25 11:27:42 +0200

    Full text search index has to filter for tenant as well

--- src/Backend.Fx.EfCorePersistence/Mssql/MsSqlFullTextSearchService.cs
+++ src/Backend.Fx.EfCorePersistence/Mssql/MsSqlFullTextSearchService.cs
@@ -3,24 +3,29 @@
     using System.Linq;
     using BuildingBlocks;
     using EfCorePersistence;
+    using Environment.MultiTenancy;
     using Microsoft.EntityFrameworkCore;
     using Patterns.Authorization;
+    using Patterns.DependencyInjection;
 
     public class MsSqlFullTextSearchService<TAggregateRoot> : IFullTextSearchService<TAggregateRoot> where TAggregateRoot : AggregateRoot
     {
         private readonly EasyFts easyFts = new EasyFts();
         private readonly DbContext dbContext;
+        private readonly ICurrentTHolder<TenantId> currentTenantholder;
         private readonly IAggregateMapping<TAggregateRoot> aggregateMapping;
         private readonly IAggregateAuthorization<TAggregateRoot> aggregateAuthorization;
         private readonly string schema;
         private readonly string table;
-        
+
         public MsSqlFullTextSearchService(
-            DbContext dbContext, 
-            IAggregateMapping<TAggregateRoot> aggregateMapping, 
+            DbContext dbContext,
+            ICurrentTHolder<TenantId> currentTenantholder,
+            IAggregateMapping<TAggregateRoot> aggregateMapping,
             IAggregateAuthorization<TAggregateRoot> aggregateAuthorization)
         {
             this.dbContext = dbContext;
+            this.currentTenantholder = currentTenantholder;
             this.aggregateMapping = aggregateMapping;
             this.aggregateAuthorization = aggregateAuthorization;
 
@@ -42,8 +47,14 @@
             else
             {
                 var sql = $"SELECT * FROM [{schema}].[{table}] WHERE Contains ({table}.*, '{ftsQuery}')";
+#pragma warning disable EF1000 // Possible SQL injection vulnerability.
                 queryable = aggregateAuthorization.Filter(dbContext.Set<TAggregateRoot>().FromSql(sql));
+#pragma warning restore EF1000 // Possible SQL injection vulnerability.
             }
+
+            queryable = currentTenantholder.Current.HasValue
+                                                           ? queryable.Where(agg => agg.TenantId == currentTenantholder.Current.Value)
+                                                           : queryable.Where(agg => false);
 
             foreach (var includeDefinition in aggregateMapping.IncludeDefinitions)
             {
