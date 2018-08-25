3bedcb4269ee06dba403c10a5419f638e6a05250
Marc Wittke <marc@wittke-web.de>
2018-08-25 08:55:59 -0300

    new: RandomOrDefault optimized random for queryables

--- src/Backend.Fx/RandomData/LinqExtensions.cs
+++ src/Backend.Fx/RandomData/LinqExtensions.cs
@@ -3,6 +3,9 @@
     using System;
     using System.Collections.Generic;
     using System.Linq;
+    using System.Linq.Dynamic.Core;
+    using System.Reflection;
+    using BuildingBlocks;
 
     public static class LinqExtensions
     {
@@ -40,18 +43,78 @@
                 throw new ArgumentNullException(nameof(source));
             }
 
-            if (source is IQueryable<T> sourceQueryable)
+            // ReSharper disable once PossibleMultipleEnumeration
+            if (TryAsQueryable(source, out IQueryable<T> sourceQueryable, out int count))
             {
-                int count = sourceQueryable.Count();
+                if (count == 0)
+                {
+                    throw new ArgumentException(string.Format("The enumerable of {0} does not contain any items, therefore no random item can be returned.", typeof(T).Name), nameof(source));
+                }
+                
                 return sourceQueryable.Skip(TestRandom.Next(count - 1)).First();
             }
-            
+
+            // ReSharper disable once PossibleMultipleEnumeration
             var sourceArray = source as T[] ?? source.ToArray();
             if (sourceArray.Length == 0)
             {
                 throw new ArgumentException(string.Format("The enumerable of {0} does not contain any items, therefore no random item can be returned.", typeof(T).Name), nameof(source));
             }
             return sourceArray.ElementAt(TestRandom.Next(sourceArray.Length));
+        }
+
+        public static T RandomOrDefault<T>(this IEnumerable<T> source)
+        {
+            if (source == null)
+            {
+                throw new ArgumentNullException(nameof(source));
+            }
+
+            // ReSharper disable once PossibleMultipleEnumeration
+            if (TryAsQueryable(source, out IQueryable<T> sourceQueryable, out int count))
+            {
+                if (count == 0)
+                {
+                    return default(T);
+                }
+                
+                return sourceQueryable.Skip(TestRandom.Next(count - 1)).FirstOrDefault();
+            }
+
+            // ReSharper disable once PossibleMultipleEnumeration
+            var sourceArray = source as T[] ?? source.ToArray();
+            if (sourceArray.Length == 0)
+            {
+                return default(T);
+            }
+            return sourceArray.ElementAt(TestRandom.Next(sourceArray.Length));
+        }
+
+        private static bool TryAsQueryable<T>(this IEnumerable<T> source, out IQueryable<T> outQueryable, out int count)
+        {
+            outQueryable = null;
+            count = 0;
+
+            if (source is IQueryable<T> sourceQueryable)
+            {
+                count = sourceQueryable.Count();
+                if (count == 0)
+                {
+                    outQueryable = new T[0].AsQueryable();
+                    return true;
+                }
+                
+                var idProperty = typeof(T).GetProperty(nameof(Entity.Id), BindingFlags.Instance | BindingFlags.Public);
+                if (idProperty != null)
+                {
+                    sourceQueryable = sourceQueryable.OrderBy(nameof(Entity.Id));
+                }
+
+                outQueryable = sourceQueryable;
+                return true;
+            }
+
+            return false;
         }
     }
 }
