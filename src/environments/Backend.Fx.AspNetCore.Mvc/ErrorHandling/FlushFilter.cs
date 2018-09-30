72560cec496231d27aef521c090215243b88f359
Marc Wittke <marc@wittke-web.de>
2018-09-30 10:05:48 -0300

    FlushFilter added again. Still necesary because of MVC design

---
+++ src/environments/Backend.Fx.AspNetCore.Mvc/ErrorHandling/FlushFilter.cs
@@ -0,0 +1,35 @@
+﻿using System.Diagnostics;
+using Backend.Fx.Patterns.DependencyInjection;
+using Backend.Fx.Patterns.UnitOfWork;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace Backend.Fx.AspNetCore.Mvc.ErrorHandling
+{
+    /// <summary>
+    /// Enforces flushing of db changes when an action was executed, so that possible schema violation errors
+    /// are raised before the http response starts, and our exception handling can still change the response.
+    /// </summary>
+    /// <remarks>This cannot be done in a middleware. MVC will start the response as soon as the controller
+    /// action returns. The <see cref="Backend.Fx.AspNetCore.Scoping.UnitOfWorkMiddleware"/> would commit changes
+    /// too late for the <see cref="Backend.Fx.AspNetCore.ErrorHandling.JsonErrorHandlingMiddleware"/> to
+    /// manipulate the response status.</remarks>
+    [DebuggerStepThrough]
+    public class FlushFilter : IActionFilter
+    {
+        private readonly IBackendFxApplication _backendFxApplication;
+
+        public FlushFilter(IBackendFxApplication backendFxApplication)
+        {
+            _backendFxApplication = backendFxApplication;
+        }
+
+        public void OnActionExecuting(ActionExecutingContext context)
+        { }
+
+        public void OnActionExecuted(ActionExecutedContext context)
+        {
+            // that's all:
+            _backendFxApplication.CompositionRoot.GetInstance<ICanFlush>().Flush();
+        }
+    }
+}
