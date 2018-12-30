7db28025bf3830774fef1fcaaffc6c9a8d8b051b
Marc Wittke <marc@wittke-web.de>
2018-12-30 17:28:24 +0000

    serializer settings available in virtual method

--- src/environments/Backend.Fx.AspNetCore/ErrorHandling/JsonErrorHandlingMiddleware.cs
+++ src/environments/Backend.Fx.AspNetCore/ErrorHandling/JsonErrorHandlingMiddleware.cs
@@ -17,11 +17,12 @@
     {
         private readonly IHostingEnvironment _env;
         private static readonly ILogger Logger = LogManager.Create<JsonErrorHandlingMiddleware>();
-        private readonly JsonSerializerSettings _jsonSerializerSettings = new JsonSerializerSettings
+
+        protected JsonSerializerSettings JsonSerializerSettings { get; } = new JsonSerializerSettings
         {
             ContractResolver = new DefaultContractResolver
             {
-                NamingStrategy = new CamelCaseNamingStrategy { ProcessDictionaryKeys = true }
+                NamingStrategy = new CamelCaseNamingStrategy {ProcessDictionaryKeys = true}
             },
         };
 
@@ -67,8 +68,8 @@
             }
             context.Response.StatusCode = (int) HttpStatusCode.InternalServerError;
             var responseContent = _env.IsDevelopment()
-                                          ? JsonConvert.SerializeObject(new { message = exception.Message, stackTrace = exception.StackTrace }, _jsonSerializerSettings)
-                                          : JsonConvert.SerializeObject(new { message = "An internal error occured" }, _jsonSerializerSettings);
+                                          ? JsonConvert.SerializeObject(new { message = exception.Message, stackTrace = exception.StackTrace }, JsonSerializerSettings)
+                                          : JsonConvert.SerializeObject(new { message = "An internal error occured" }, JsonSerializerSettings);
             context.Response.ContentType = "application/json; charset=utf-8";
             await context.Response.WriteAsync(responseContent);
         }
@@ -76,7 +77,7 @@
         protected virtual string SerializeErrors(Errors errors)
         {
             var errorsDictionaryForJson = errors.ToDictionary(kvp => kvp.Key == "" ? "_error" : kvp.Key, kvp => kvp.Value);
-            return JsonConvert.SerializeObject(errorsDictionaryForJson, _jsonSerializerSettings);
+            return JsonConvert.SerializeObject(errorsDictionaryForJson, JsonSerializerSettings);
         }
     }
 }
