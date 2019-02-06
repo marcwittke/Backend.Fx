f4fbe372c3e2082a8cfcc81419fa718ec173b144
Marc Wittke <marc@wittke-web.de>
2019-02-06 10:28:41 -0300

    utility classes to handle events by delegates

---
+++ src/abstractions/Backend.Fx/Patterns/EventAggregation/Integration/DelegateIntegrationEventHandler.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Backend.Fx.Patterns.EventAggregation.Integration
+{
+    public class DelegateIntegrationEventHandler<TEvent> : IIntegrationEventHandler<TEvent> where TEvent : IIntegrationEvent
+    {
+        private readonly Action<TEvent> _action;
+
+        public DelegateIntegrationEventHandler(Action<TEvent> action)
+        {
+            this._action = action;
+        }
+
+        public void Handle(TEvent eventData)
+        {
+            _action(eventData);
+        }
+    }
+
+    public class DelegateIntegrationEventHandler : IIntegrationEventHandler
+    {
+        private readonly Action<dynamic> _action;
+
+        public DelegateIntegrationEventHandler(Action<dynamic> action)
+        {
+            _action = action;
+        }
+
+        public void Handle(dynamic eventData)
+        {
+            _action(eventData);
+        }
+    }
+}
