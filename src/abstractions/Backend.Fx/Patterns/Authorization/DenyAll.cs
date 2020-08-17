2ceb4dd71a5cd894400b4058916a17fb02ad1471
Marc Wittke <marc@wittke-web.de>
2020-08-17 16:13:08 -0300

    compile fix

--- src/abstractions/Backend.Fx/Patterns/Authorization/DenyAll.cs
+++ src/abstractions/Backend.Fx/Patterns/Authorization/DenyAll.cs
@@ -1,19 +1,11 @@
 ﻿using System;
 using System.Linq.Expressions;
 using Backend.Fx.BuildingBlocks;
-using Backend.Fx.Logging;
 
 namespace Backend.Fx.Patterns.Authorization
 {
-    public sealed class DenyAll<TAggregateRoot> : AggregateAuthorization<TAggregateRoot> where TAggregateRoot : AggregateRoot
+    public class DenyAll<TAggregateRoot> : AggregateAuthorization<TAggregateRoot> where TAggregateRoot : AggregateRoot
     {
-        private static readonly ILogger Logger = LogManager.Create<AggregateAuthorization<TAggregateRoot>>();
-        
-        public DenyAll()
-        {
-            Logger.Info($"Authorization for {typeof(TAggregateRoot).Name} is set to 'deny all'");
-        }
-        
         public override Expression<Func<TAggregateRoot, bool>> HasAccessExpression
         {
             get { return agg => false; }
