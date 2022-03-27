4d3204d1888d86849e339964f78f97a3d76f3ae3
Marc Wittke <marc@wittke-web.de>
2022-03-27 07:40:55 -0300

    #124 start of sequence overridable

--- src/implementations/Backend.Fx.EfCorePersistence/Mssql/MsSqlSequence.cs
+++ src/implementations/Backend.Fx.EfCorePersistence/Mssql/MsSqlSequence.cs
@@ -12,10 +12,12 @@ namespace Backend.Fx.EfCorePersistence.Mssql
     {
         private static readonly ILogger Logger = Log.Create<MsSqlSequence>();
         private readonly IDbConnectionFactory _dbConnectionFactory;
+        private readonly int _startWith;
 
-        protected MsSqlSequence(IDbConnectionFactory dbConnectionFactory)
+        protected MsSqlSequence(IDbConnectionFactory dbConnectionFactory, int startWith = 1)
         {
             _dbConnectionFactory = dbConnectionFactory;
+            _startWith = startWith;
         }
 
         protected abstract string SequenceName { get; }
@@ -43,7 +45,7 @@ namespace Backend.Fx.EfCorePersistence.Mssql
                     Logger.LogInformation("Sequence {SchemaName}.{SequenceName} does not exist yet and will be created now", SchemaName, SequenceName);
                     using (IDbCommand cmd = dbConnection.CreateCommand())
                     {
-                        cmd.CommandText = $"CREATE SEQUENCE [{SchemaName}].[{SequenceName}] START WITH 1 INCREMENT BY {Increment}";
+                        cmd.CommandText = $"CREATE SEQUENCE [{SchemaName}].[{SequenceName}] START WITH {_startWith} INCREMENT BY {Increment}";
                         cmd.ExecuteNonQuery();
                         Logger.LogInformation("Sequence {SchemaName}.{SequenceName} created", SchemaName, SequenceName);
                     }
