e72e24844d7adb859da5ec21f0a9c8b67654f729
Marc Wittke <marc@wittke-web.de>
2022-10-07 19:22:50 -0300

    fix(IdGeneration): make it easier to base an app on a non generic IMyIdGenerator interface

--- src/abstractions/Backend.Fx/Features/IdGeneration/IdGenerationFeature.cs
+++ src/abstractions/Backend.Fx/Features/IdGeneration/IdGenerationFeature.cs
@@ -1,11 +1,36 @@
-using Backend.Fx.DependencyInjection;
 using JetBrains.Annotations;
-using Microsoft.Extensions.DependencyInjection;
 
 namespace Backend.Fx.Features.IdGeneration
 {
+    /// <summary>
+    /// use this feature type, when you want your non generic type of <see cref="IEntityIdGenerator{TId}"/> to be injected
+    /// </summary>
+    /// <typeparam name="TId"></typeparam>
     [PublicAPI]
-    public class IdGenerationFeature<TId> : Feature where TId : struct
+    public class IdGenerationFeature<TId, TIdGenerator> : Feature 
+        where TId : struct
+        where TIdGenerator : class, IEntityIdGenerator<TId> 
+    {
+        private readonly TIdGenerator _entityIdGenerator;
+
+        public IdGenerationFeature(TIdGenerator entityIdGenerator)
+        {
+            _entityIdGenerator = entityIdGenerator;
+        }
+        
+        public override void Enable(IBackendFxApplication application)
+        {
+            application.CompositionRoot.RegisterModules(new IdGenerationModule<TId, TIdGenerator>(_entityIdGenerator));
+        }
+    }
+    
+    /// <summary>
+    /// use this feature type, when you want the generic <see cref="IEntityIdGenerator{TId}"/> to be injected
+    /// </summary>
+    /// <typeparam name="TId"></typeparam>
+    [PublicAPI]
+    public class IdGenerationFeature<TId> : Feature 
+        where TId : struct
     {
         private readonly IEntityIdGenerator<TId> _entityIdGenerator;
 
@@ -16,22 +41,7 @@ namespace Backend.Fx.Features.IdGeneration
         
         public override void Enable(IBackendFxApplication application)
         {
-            application.CompositionRoot.RegisterModules(new IdGenerationModule<TId>(_entityIdGenerator));
-        }
-    }
-
-    public class IdGenerationModule<TId> : IModule where TId : struct
-    {
-        private readonly IEntityIdGenerator<TId> _entityIdGenerator;
-
-        public IdGenerationModule(IEntityIdGenerator<TId> entityIdGenerator)
-        {
-            _entityIdGenerator = entityIdGenerator;
-        }
-
-        public void Register(ICompositionRoot compositionRoot)
-        {
-            compositionRoot.Register(ServiceDescriptor.Singleton(_entityIdGenerator));
+            application.CompositionRoot.RegisterModules(new IdGenerationModule<TId, IEntityIdGenerator<TId>>(_entityIdGenerator));
         }
     }
 }
