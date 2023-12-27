83cb4cdf344c72302c0127cf24bbc7b623024760
Marc Wittke <marc@wittke-web.de>
2023-12-27 12:09:03 +0100

    Make hosted service generic (#157)
    * Make hosted service generic
    
    * inject hosted service and generic application

--- src/environments/Backend.Fx.AspNetCore/BackendFxApplicationHostedService.cs
+++ src/environments/Backend.Fx.AspNetCore/BackendFxApplicationHostedService.cs
@@ -3,22 +3,27 @@ using System.Threading;
 using System.Threading.Tasks;
 using Backend.Fx.Logging;
 using Backend.Fx.Patterns.DependencyInjection;
+using JetBrains.Annotations;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using ILogger = Microsoft.Extensions.Logging.ILogger;
 
 namespace Backend.Fx.AspNetCore
 {
-    public interface IBackendFxApplicationHostedService : IHostedService
+    [PublicAPI]
+    public interface IBackendFxApplicationHostedService<out TApplication> : IHostedService
+        where TApplication : IBackendFxApplication
     {
-        IBackendFxApplication Application { get; }
+        TApplication Application { get; }
     }
-    
-    public abstract class BackendFxApplicationHostedService : IBackendFxApplicationHostedService
-    {
-        private static readonly ILogger Logger = Log.Create<BackendFxApplicationHostedService>();
 
-        public abstract IBackendFxApplication Application { get; }
+    public abstract class BackendFxApplicationHostedService<TApplication> : IBackendFxApplicationHostedService<TApplication>
+        where TApplication : IBackendFxApplication
+    {
+        private static readonly ILogger Logger = Log.Create<BackendFxApplicationHostedService<TApplication>>();
+
+        public abstract TApplication Application { get; }
 
         public virtual async Task StartAsync(CancellationToken ct)
         {
@@ -43,6 +48,22 @@ namespace Backend.Fx.AspNetCore
                 Application.Dispose();
                 return Task.CompletedTask;
             }
+        }
+    }
+    
+    public static class BackendFxApplicationHostedServiceExtensions
+    {
+        public static void AddBackendFxApplication<THostedService, TApplication>(this IServiceCollection services)
+            where THostedService : class, IBackendFxApplicationHostedService<TApplication>
+            where TApplication : class, IBackendFxApplication
+        {
+            services.AddSingleton<THostedService>();
+            
+            // this registration ensures starting of the hosted service
+            services.AddSingleton<IHostedService>(provider => provider.GetRequiredService<THostedService>());
+
+            // this registration makes the application instance available as singleton
+            services.AddSingleton(provider => provider.GetRequiredService<THostedService>().Application);
         }
     }
 }
